cf0fdaf9d7085d4a31124cf189aa87c18941f306
a <102840087+letter-aa@users.noreply.github.com>
2023-11-05 15:22:15 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -17,59 +17,8 @@ using namespace std;
 #define DOWN_ARROW 80
 string startInput(string preMessage)
 {
-	string input;
-	int rowPos = 0;
-	int linePos = 0;
-
-	for (int i = 0;;) {
-		i = _getch();
-		if (i == 8) {
-			if (rowPos > 0) {
-				rowPos = rowPos - 1;
-				cout << "\b \b";
-				input.pop_back();
-			}
-		}
-		else {
-			switch (i) {
-			case ANY_ARROW:
-				i = _getch();
-				switch (i) {
-				case LEFT_ARROW:
-					cout << "\b";
-					if (rowPos > 0) {
-						rowPos = rowPos - 1;
-					}
-					break;
-				case RIGHT_ARROW:
-					if (rowPos < input.length()) {
-						cout << "\033[C"; //idk how tf this works whoever's reading this pls explain!11
-						rowPos = rowPos + 1;
-					}
-					break;
-				case UP_ARROW:
-					break;
-				case DOWN_ARROW:
-					break;
-				}
-				break;
-			default:
-				if (rowPos == input.size()) {
-					input = input + (char)i;
-					cout << "\r" << input;
-					rowPos = rowPos + 1;
-				}
-				else {
-					input = input.substr(0, rowPos) + (char)i + input.substr(rowPos);
-					cout << "\r" << input;
-					for (int i = 0; i < input.size() - rowPos; i++) {
-						cout << "\b";
-					}
-					rowPos = rowPos + 1;
-				}
-			}
-		}
-	}
+	cout << preMessage;
+	//insert type-test repository code here (the code inside the main function)
 	return input;
 }
 /////////////////////////////////////////////////////////////////////////////
